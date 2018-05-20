261ea335b9f64a6a9f57a4e3dde544810234c37e
Jimck <1280033048@qq.com>
2018-05-20 19:51:16 +0800

    添加home login register 完善博客模块

---
+++ src/containers/HomeContainerLogin/index.js
@@ -0,0 +1,25 @@
+/**
+ *创建时间:  2018/5/20
+ *  作  者：Jimck_Zhang
+ *  邮  箱：XIAOZHANG10086XIAOZHANG@live.com
+ *  功  能:
+ */
+
+import React from 'react';
+
+class HomeLogin extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>404 not found page</h1>
+      </div>
+    );
+  }
+}
+
+export default HomeLogin;
